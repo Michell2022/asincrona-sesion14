fe6a3645ebcac61941a783ceb61dbc08f482ba73
Michell2022 <sistemas.angel04@gmail.com>
2023-01-10 23:02:04 -0500

    Estilos y Logica terminado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  
+  verificador = true;
+
+  // VARIABLE QUE ENVIARA MI OBJETO AL COMPONENTE HIJO
+  subtitulo!:string;
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      nombre: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern('[a-zA-Z ]*')
+        ]
+      ],
+      dni: ['',
+        [
+          Validators.required,
+        ]
+      ],
+      estado_civil: ['',
+        [
+          Validators.required,
+        ]
+      ]
+    })
+  }
+
+  send(): any {
+    this.subtitulo = (this.formLogin.value);
+    this.verificador = true;
+  }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { ComponentsModule } from '../components/components.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +11,11 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    ReactiveFormsModule,
+    CommonModule, ComponentsModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class PagesModule { }
